37c3962d14009fe5a2fcf09d9fe50255aaa16895
h-yes-oo <hyesoo5115@naver.com>
2021-06-21 11:15:23 +0900

    Math.ceil

--- javascript/section1/3.js
+++ javascript/section1/3.js
@@ -2,13 +2,9 @@ const fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split(' ');
 let a = parseInt(input[0]);
 
-const sum = (n) => {
-  let total = n;
-  for(let i = 1; i < n; i++){
-    total += i;
-  }
-  return total;
+function solution(a) {
+  let sol = Math.ceil(a/12);
+  return sol;
 }
 
-console.log(sum(a));
-
+console.log(solution(a));
