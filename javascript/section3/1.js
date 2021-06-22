aab0ac7ed8efa64dab640dd3535ff90e5203c6a0
h-yes-oo <hyesoo5115@naver.com>
2021-06-22 17:08:08 +0900

    use toLowerCase to string, not each letter

--- javascript/section3/1.js
+++ javascript/section3/1.js
@@ -3,8 +3,9 @@ const input = fs.readFileSync('/dev/stdin').toString().split('\n')[0];
 
 let flag = true;
 let len = input.length;
+let lowInput = input.toLowerCase();
 for(let idx = 0; idx < Math.floor(len/2); idx++){
-  if(input[idx].toUpperCase() !== input[len-1-idx].toUpperCase()){
+  if(lowInput[idx] !== lowInput[len-1-idx]){
     flag = false;
     break;
   }
