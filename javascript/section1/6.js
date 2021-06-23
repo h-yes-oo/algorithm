b7ce138a548199fb4f6b3c3fff2768cbafbbdacb
h-yes-oo <hyesoo5115@naver.com>
2021-06-23 11:37:23 +0900

    array.prototype.map, array.prototype.filter

---
+++ javascript/section1/6.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+const num = input.map(x => parseInt(x));
+
+const odd = num.filter(x => x%2 == 0 ? false : true);
+
+let min = odd[0];
+let sum = 0;
+for(let i = 0; i < odd.length; i++){
+  if(odd[i] < min) min = odd[i];
+  sum += odd[i];
+}
+
+console.log(sum);
+console.log(min);
