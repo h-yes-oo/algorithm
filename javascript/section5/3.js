903a28320d95491e186aba9d6f453028d1b5cc30
h-yes-oo <hyesoo5115@naver.com>
2021-07-28 17:58:58 +0900

    better idea - only one for loop

--- javascript/section5/3.js
+++ javascript/section5/3.js
@@ -4,15 +4,33 @@ const input = fs.readFileSync('/dev/stdin').toString().split('\n');
 const [n,m] = input[0].split(' ').map(x => parseInt(x));
 const list = input[1].split(' ').map(x => parseInt(x));
 
-
-let answer = 0;
-for(let start = 0; start < n; start++){
-  let sum = 0;
-  let end = start;
-  while(sum < m && end < n){
-    sum += list[end++];
-    if(sum === m) answer++;
+function sol1(n,m, list){
+  let answer = 0;
+  for(let start = 0; start < n; start++){
+    let sum = 0;
+    let end = start;
+    while(sum < m && end < n){
+      sum += list[end++];
+      if(sum === m) answer++;
+    }
   }
+  return answer;
 }
 
-console.log(answer);
+function sol2(n, m, list){
+  let answer = 0;
+  let sum = 0;
+  let start = 0;
+  for(let end = 0; end < n; end++){
+    sum += list[end];
+    if(sum === m) answer++;
+    while(sum >= m){
+      sum -= list[start++];
+      if(sum === m) answer++;
+    }
+  }
+  return answer;
+}
+
+console.log(sol1(n,m,list));
+console.log(sol2(n,m,list));
